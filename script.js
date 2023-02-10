dcd50b0a4e64fc5dc7268677cfa58938ded1d279
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-02-10 22:30:45 +0100

    wrote code for starting condition

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 'use strict';
-// 
+
+// selected elements for DOM manipulation
 const score0El = document.getElementById('score--0') ;
 const score1El = document.querySelector('#score--1') ;
+const diceEl = document.querySelector('.dice');
+
+// starting conditions
+diceEl.classList.add('hidden');
+score0El.textContent = 0 ;
+score1El.textContent = 0 ;

13eba0aa97ca983558ace832555777e9bb84c2c5
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-02-10 22:49:01 +0100

    added event listener to the roll btn

--- script.js
+++ script.js
@@ -4,8 +4,22 @@
 const score0El = document.getElementById('score--0') ;
 const score1El = document.querySelector('#score--1') ;
 const diceEl = document.querySelector('.dice');
+const rollBtn = document.querySelector('.btn--roll');
+const newBtn = document.querySelector('.btn--new');
+const holdBtn = document.querySelector('.btn--hold');
 
 // starting conditions
 diceEl.classList.add('hidden');
 score0El.textContent = 0 ;
 score1El.textContent = 0 ;
+
+// add rollBtn functionality
+rollBtn.addEventListener('click', function(){
+    // 1 - generate a random number
+    const dice = Math.floor(Math.random() * 6) + 1;
+    // we can also use Math.trunc instead of Math.floor to convert the decimal number to a whole number.
+    console.log(dice);
+    // 2- display hidden class
+    // so we want to display the dice when we hit the roll button
+    diceEl.classList.remove('hidden');
+});

624a1f7ab72bd50dc70bc5bcbbdff023fabfd4cc
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-02-11 00:08:44 +0100

    created an if-else sentence

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const diceEl = document.querySelector('.dice');
 const rollBtn = document.querySelector('.btn--roll');
 const newBtn = document.querySelector('.btn--new');
 const holdBtn = document.querySelector('.btn--hold');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
+
+// initialize currentScore
+let currentScore = 0;
 
 // starting conditions
 diceEl.classList.add('hidden');
@@ -22,4 +27,13 @@ rollBtn.addEventListener('click', function(){
     // 2- display hidden class
     // so we want to display the dice when we hit the roll button
     diceEl.classList.remove('hidden');
+    // so we want to dynamically load one of the six images
+    diceEl.src = `dice-${dice}.png`;
+    // 3 - if the dice = 1, switch to next player
+    if(dice !== 1){
+        // when the dice = 1
+        currentScore += dice;
+    }else{
+        // when the dice is not = 1
+    }
 });
